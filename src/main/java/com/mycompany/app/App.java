e3b933ea82d9b9eb73e30833b9e4007da2274df7
Rares Smeu <Rares.Smeu@iquestgroup.com>
2016-11-09 16:40:44 +0200

    This won't compile

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+	this wont compile :)     
+        System.out.println( "Hello World!" );
+    }
+}
